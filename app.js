9f5ed4e504bcbc11e49d96565c99b9fc0c20c064
Youness Dradeb <youness.dradeb@gmail.com>
2025-02-05 15:53:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const app = express();
+const PORT = process.env.PORT || 80;
+const rateLimit = require("express-rate-limit");
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var smtpConfig = {
+  host: 'mail.altorcrossfit.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+      user: 'form@altorcrossfit.com',
+      pass: process.env.PASS
+  }
+};
+var transporter = nodemailer.createTransport(smtpConfig);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+// API endpoint to send emails
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  const mailOptions = {
+    from: 'form@altorcrossfit.com', // Sender address
+    to : 'contact@altorcrossfit.com', // Recipient address
+    subject : 'New contact', // Email subject
+    text, // Email body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
