6b871b8769a65ef68b99a9d079218fb9dc5468df
Youness Dradeb <youness.dradeb@gmail.com>
2025-02-05 15:49:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+                                                                                     index.js                                                                                                 
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
+const app = express();
+const PORT = process.env.PORT || 80;
+const rateLimit = require("express-rate-limit");
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
+
+var smtpConfig = {
+  host: 'mail.altorcrossfit.com',
+  port: 465,
+  secure: true, // use SSL
+  auth: {
+      user: 'form@altorcrossfit.com',
+      pass: process.env.PASS
   }
+};
+var transporter = nodemailer.createTransport(smtpConfig);
+
+
+// API endpoint to send emails
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from: 'form@altorcrossfit.com', // Sender address
+    to : 'contact@altorcrossfit.com', // Recipient address
+    subject : 'New contact', // Email subject
+    text, // Email body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
